aef45fab575f2b4562811fbed635b6c8e8dd6dbf
engmacieira <eng.macieira@gmail.com>
2025-10-24 19:23:14 -0300

    commit
    Nova Versão do Sistema - MVC-POO-FastAPI.v1.2

--- app/core/security.py
+++ app/core/security.py
@@ -1,18 +1,22 @@
 import os
+import logging
 from datetime import datetime, timedelta
 from jose import JWTError, jwt
 from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
-from psycopg2.extensions import connection  # Para type hint
+from psycopg2.extensions import connection 
 from app.models.user_model import User
 from app.repositories.user_repository import UserRepository
 from app.core.database import get_db
+
+logger = logging.getLogger(__name__)
 
 SECRET_KEY = os.environ.get("SECRET_KEY")
 ALGORITHM = os.environ.get("ALGORITHM", "HS256")
 ACCESS_TOKEN_EXPIRE_MINUTES = int(os.environ.get("ACCESS_TOKEN_EXPIRE_MINUTES", 30))
 
 if not SECRET_KEY:
+    logger.critical("Variável de ambiente SECRET_KEY não definida!")
     raise ValueError("Variável de ambiente SECRET_KEY não definida. Verifique seu .env")
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/login")
@@ -33,6 +37,7 @@ def create_access_token(user: User) -> str:
 def get_current_user(
     token: str = Depends(oauth2_scheme), 
     db_conn: connection = Depends(get_db)) -> User:
+    
     credentials_exception = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
         detail="Não foi possível validar as credenciais",
@@ -41,12 +46,30 @@ def get_current_user(
     try:
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
         username: str = payload.get("sub")
-        if username is None:
+        user_id: int = payload.get("id") # Boa prática pegar o ID também
+        if username is None or user_id is None:
             raise credentials_exception
     except JWTError:
         raise credentials_exception 
+    
     user_repo = UserRepository(db_conn)
-    user = user_repo.get_by_username(username=username)
+    user = user_repo.get_by_id(user_id=user_id)
+    
     if user is None:
         raise credentials_exception
     return user
+
+#NIVEL DE ACESSO
+def require_access_level(required_level: int):
+    async def check_permission(current_user: User = Depends(get_current_user)) -> User:
+        if current_user.nivel_acesso > required_level:
+            logger.warning(f"Acesso negado para usuário '{current_user.username}' (Nível {current_user.nivel_acesso}) à recurso de Nível {required_level}.")
+            raise HTTPException(
+                status_code=status.HTTP_403_FORBIDDEN,
+                detail="Você não tem permissão para realizar esta ação."
+            )
+        return current_user 
+    
+require_admin = require_access_level(1)
+require_editor = require_access_level(2)
+require_viewer = require_access_level(3)
